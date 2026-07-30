d2f4fe26f18f76275869ae2fc9aa844bccf3d28e
Mario Loaiza Sovero <mario.loaiza@coderslab.io>
2026-07-30 01:15:45 +0000

    SSR Compute Role

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,6 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { createBridgeStateTable } from "./custom/bridge-state/resource";
+import { createSsrComputeRole } from "./custom/ssr-compute-role/resource";
 import { cognitoTokenExchange } from "./functions/cognito-token-exchange/resource";
 
 const backend = defineBackend({
@@ -9,10 +10,20 @@ const backend = defineBackend({
 const bridgeStack = backend.createStack("identity-bridge-state");
 const stateTable = createBridgeStateTable(bridgeStack);
 
+const runtimeAccessStack = backend.createStack(
+  "identity-bridge-runtime-access",
+);
+const ssrComputeRole = createSsrComputeRole(runtimeAccessStack, {
+  stateTable,
+  tokenExchangeFunction:
+    backend.cognitoTokenExchange.resources.lambda,
+});
+
 backend.addOutput({
   custom: {
     bridgeStateTableName: stateTable.tableName,
     cognitoTokenExchangeFunctionName:
       backend.cognitoTokenExchange.resources.lambda.functionName,
+    ssrComputeRoleArn: ssrComputeRole.roleArn,
   },
 });
