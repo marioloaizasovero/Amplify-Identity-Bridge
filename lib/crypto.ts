2619f2caace5ca8ec5e6b0b9cb998edd56bf7e1f
Mario Loaiza <36822946+marioloaizasovero@users.noreply.github.com>
2026-08-03 20:22:21 -0400

    Fix bug after to merge (#9)
    Co-authored-by: Mario Loaiza <mario.loaiza@coderslab.io>

--- lib/crypto.ts
+++ lib/crypto.ts
@@ -6,6 +6,13 @@ export function randomOpaqueToken(byteLength = 32) {
   }
 
   return randomBytes(byteLength).toString("base64url");
+} 
+export function createState() {
+  return randomOpaqueToken(32);
+}
+
+export function createNonce() {
+  return randomOpaqueToken(32);
 }
 
 export function createSessionId() {
